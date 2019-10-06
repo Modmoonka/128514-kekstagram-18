181f554c1e39205ae4452312fd09706055bd2260
Modmoonka <tanya-mongush@mail.ru>
2019-10-06 13:20:40 +0700

    module4-task2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 'use strict';
 
 var COUNT_ARRAY = 25;
-
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 var DESCRIPTION_PICTURE = ['Классное фото', 'Мой отдых', 'Это мой питомец'];
 var NAMES_AVTOR = ['Кеша Лысый', 'Аркадий Паровозкин', 'Инокентий Джигурда', 'Яков Великий', 'Тула Мула', 'Мося Мосянин'];
 var MASSAGE = ['Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше', 'В целом всё неплохо. Но не всё.'];
 
 var descriptionFoto = [];
 
-var picture = document.querySelector('.pictures');
+var getQuery = function (argumentQuery) {
+  return document.querySelector(argumentQuery);
+};
+
+var picture = getQuery('.pictures');
 var fragment = document.createDocumentFragment();
 
 var templatePicture = document.querySelector('#picture')
@@ -89,4 +94,72 @@ var showBigFoto = function (object) {
 };
 
 showDescriptionFoto();
-showBigFoto(descriptionFoto[0]);
+//showBigFoto(descriptionFoto[0]);
+
+
+//Загрузка изображения и показ формы редактирования
+
+getQuery('#upload-file').addEventListener('change', function () {
+  getQuery('.img-upload__overlay').classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+});
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var closePopup = function () {
+  getQuery('.img-upload__overlay').classList.add('hidden');
+  getQuery('#upload-file').value('');
+};
+
+getQuery('.img-upload__cancel').addEventListener('click', function () {
+  closePopup();
+});
+
+getQuery('.img-upload__cancel').addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+getQuery('.img-upload__cancel').addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+});
+
+//Применение эффекта для изображения и Редактирование размера изображения
+getQuery('.effect-level__line').addEventListener('mouseup', function (evt) {
+  writeCoordinatLevelPin(evt);
+});
+
+function writeCoordinatLevelPin(evt) {
+  //var startCoordinat = getQuery('.effect-level__line').offsetLeft +  getQuery('.img-upload__wrapper').offsetLeft + 45;
+  var startCoordinat = (getQuery('.img-upload__preview').offsetWidth - getQuery('.effect-level__line').offsetWidth) / 2  + getQuery('.img-upload__wrapper').offsetLeft;
+  //var persentLevelPin = Math.round((evt.clientX - startCoordinat) * 100 / 455);
+
+  // if (persentLevelPin > 100) {
+  //   persentLevelPin = 100;
+  // }
+  // if (persentLevelPin < 0) {
+  //   persentLevelPin = 0;
+  // }
+  // console.log(document.querySelector('.effect-level__pin').style.left);
+ // getQuery('.effect-level__pin').style.left = evt.clientX;
+console.log(startCoordinat);
+  console.log(evt.clientX);
+}
+
+
+//Валидация хеш-тегов
+
+validHashTag('#1231111#1111111#88888jjjjj');
+
+function validHashTag(text) {
+  var arrayHashTags = text.split('#');
+  arrayHashTags.splice(0, 1);
+}
+
