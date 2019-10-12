2e9d9b48045433a5a8c56f48c8f646788c2ddf99
Modmoonka <tanya-mongush@mail.ru>
2019-10-12 16:29:17 +0700

    done module5-task2

---
+++ js/description-foto.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function () {
+  var picture = document.querySelector('.pictures');
+  var templatePicture = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
+
+  var createTemplatePicture = function (template, object) {
+    var cloneElement = template.cloneNode(true);
+    cloneElement.querySelector('.picture__img').src = object.url;
+    cloneElement.querySelector('.picture__likes').textContent = object.likes;
+    cloneElement.querySelector('.picture__comments').textContent = object.comments.length;
+    return cloneElement;
+  };
+
+  var showDescriptionFoto = function () {
+    // window.util.createDescriptionFoto();
+    for (var i = 1; i <= window.util.COUNT_PICTURES_ON_PAGE; i++) {
+      window.util.fragment.appendChild(createTemplatePicture(templatePicture, window.util.descriptionFoto[i - 1])).accessKey = i;
+    }
+    picture.appendChild(window.util.fragment);
+  };
+
+  showDescriptionFoto();
+})();
